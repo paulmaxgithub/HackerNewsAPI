9b88d893a9b5fa3007ace7e5132fc2e5d429ddbe
paulmax <ipaulmax@icloud.com>
2022-12-12 01:25:11 +0300

    Initial Commit

---
+++ HackerNewsAPI/HackerNewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAPIApp.swift
+//  HackerNewsAPI
+//
+//  Created by PaulmaX on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
